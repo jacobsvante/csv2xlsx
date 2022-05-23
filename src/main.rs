14291b523c86ff88badea66c61f6eeb7b1b74712
Jacob Magnusson <m@jacobian.se>
2022-05-23 12:15:43 +0200

    fix: Couldn't write to explicitly provided filename

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use csv2xlsx::{cli, csv2xlsx};
-use std::{
-    fs::File,
-    io::{stdout, Write},
-};
+use std::{fs::File, io::Write};
 
 fn main() -> anyhow::Result<()> {
     let opts = cli::parse();
-    let out = csv2xlsx(
+    let data = csv2xlsx(
         File::open(opts.input_file)?,
         Some(opts.delimiter),
         opts.width_adjustment,
     )?;
-    stdout().write_all(&out)?;
+    let mut out = File::options()
+        .write(true)
+        .create(true)
+        .open(opts.output_file)?;
+    out.write_all(&data)?;
     Ok(())
 }
